a808462f7c09496213af56ab397efbaea3614425
LiamPettigrew <80730203+LiamPettigrew@users.noreply.github.com>
2021-06-17 22:53:58 +0000

    Added phase 2, which asks the user if they wish to either play the quiz or view the information documents. It then shows the list of documents/levels and asks which they wish to view/play.

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 
 def main():
-  # Code will continue here
-  print("test")
+  """
+  Phase 2 - v1.0
+  Authorised by Liam Pettigrew
+  18th June 2021
+
+  The user is asked if they wish to either view the education documents, or continue to the quiz levels.
+  """
+  print("How do you wish to continue?")
+  print(" - To view the Te Reo Maaori education documents, please type: Documents")
+  print(" - To view the levels of the quiz, please type: Quiz")
+  cont2 = str(input())
+  if cont2 == "Documents" or cont2 == "documents":
+    print("All documents containing the Te Reo Maaori information correlating to the quiz levels:")
+    print("Document 1 - ") # Document title goes here
+    print("Document 2 - ") # Document title goes here
+    print("Document 3 - ") # Document title goes here
+    print("Document 4 - ") # Document title goes here
+    print("Document 5 - ") # Document title goes here
+    print("Document 6 - ") # Document title goes here
+  elif cont2 == "Quiz" or cont2 == "quiz":
+    print("All quiz levels:")
+    print("Level 1 - ") # Level title goes here (the same as the document title)
+    print("Level 2 - ") # Level title goes here (the same as the document title)
+    print("Level 3 - ") # Level title goes here (the same as the document title)
+    print("Level 4 - ") # Level title goes here (the same as the document title)
+    print("Level 5 - ") # Level title goes here (the same as the document title)
+    print("Level 6 - ") # Level title goes here (the same as the document title)
+    print()
+    print(" - To play the level of your choice, please type: [Number]...")
+  elif cont2 != "Documents" or cont2 != "documents" or cont2 != "quiz" or cont != "Quiz":
+    print("Sorry " + cont2 + " is not a valid answer. Please try again... ")
+    print()
+    main()
 
 """
 Starting Welcome - v1.0
