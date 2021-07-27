0b1d8e998452eaf092c06ca7178324343b69674d
LiamPettigrew <80730203+LiamPettigrew@users.noreply.github.com>
2021-07-27 02:42:47 +0000

    COMMIT 7 - The questions and answers (commented) have been added in the quiz function. However, no answer-checking functions have been added yet.

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def main():
         print()
         print("Me haere koe ki te kāinga - You should go home")
         print("Me whakarongo koe ki tōu whaea - You should listen to your aunty")
-        print("Me horoi ngā kākahu! - You should wash your clothes")
+        print("Me horoi ngā kākahu - You should wash your clothes")
         print()
         print("SIMPLE PASSOVE FORM INCLUDING 'LETS' (EXAMPLES):")
         print()
@@ -203,7 +203,7 @@ def main():
         print("STATEMENTS VS QUESTIONS:")
         print()
         print("Kei te whiti te rā - The sun is shining.")
-        print(" > Kei te whiti te rā? - Kei te whiti te rā?")
+        print(" > Kei te whiti te rā? - Is the sun shining?")
         print()
         print("Kei roto te kai i te umu - The food is in the oven")
         print(" > Kei roto te kai i te umu? - Is the food in the oven?")
@@ -241,23 +241,225 @@ def main():
         print("Nōnahea koe i hoki mai ai?                                   When did you return?")
         docend()
       elif documentlevel != range(1,10):
-        print("Sorry, that is not a valid level.")
+        print("Sorry, that is not a valid document level.")
         print("Make sure you are typing only the number of the level.")
         docs()
     docs()
     
   elif cont2 == "Quiz" or cont2 == "quiz":
+    """
+    Phase Five - v1.0
+    Authorised by Liam Pettigrew
+    6th July 2021
+
+    This is the quiz function, where the titles for the quizzes are displayed. The user
+    is also asked which level they wish to view, and it then runs the level.
+    """
     # (Commit 6) a proper function has been added for the quiz.
+    # (Commit 7) the answers for the questions have been commented, but they do not respond.
     def quiz():
       print("All quiz levels:")
-      print("Level 1 - ") # Level title goes here (the same as the document title)
-      print("Level 2 - ") # Level title goes here (the same as the document title)
-      print("Level 3 - ") # Level title goes here (the same as the document title)
-      print("Level 4 - ") # Level title goes here (the same as the document title)
-      print("Level 5 - ") # Level title goes here (the same as the document title)
-      print("Level 6 - ") # Level title goes here (the same as the document title)
+      print("Level 1 - Maori Greetings + Farewells")
+      print("Level 2 - Maori Objects")
+      print("Level 3 - Maori Places/Geography")
+      print("Level 4 - Maori Proverbs")
+      print("Level 5 - Maori Sentences - Commands")
+      print("Level 6 - Maori Sentences - Questions")
       print()
       print(" - To play the level of your choice, please type: [Number]...")
+      quizlevel = int(input())
+      """
+      Phase Seven - v1.0
+      Authorised by Liam Pettigrew
+      27th July 2021
+
+      All questions for the quiz function are listed here, including their answers.
+      The questions and answers have been found from reliable sources (the same sources the
+      information from the documents was gathered from).
+      """
+      if quizlevel == 1:
+        print("Maori Greetings + Farewells quiz has begun.")
+        print()
+        print("QUESTION 1")
+        print("How do you say 'Hello' is Te Reo Maori?")
+        # Kia Ora
+        print()
+        print("QUESTION 2")
+        print("How do you formally say 'Hello' in Te Reo Maori?")
+        # Tēnā koe
+        print()
+        print("QUESTION 3")
+        print("How do you say 'Hello' to three or more people in Te Reo Maori?")
+        # Kia ora kōrua
+        print()
+        print("QUESTION 4")
+        print("How would you ask 'How is it going' to someone in Te Reo Maori?")
+        # Kei te pēhea koe?
+        print()
+        print("QUESTION 5")
+        print("How would you respond to someone by saying 'Really good' in Te Reo Maori?")
+        # Tino pai
+        print()
+        print("QUESTION 6")
+        print("How do you welcome someone in Te Reo Maori?")
+        # Nau mai, haere mai
+        print()
+        print("QUESTION 7")
+        print("How do you say 'Goodbye' in Te Reo Maori?")
+        # Haere rā
+      elif quizlevel == 2:
+        print("Maori Objects quiz has begun.")
+        print()
+        print("QUESTION 1")
+        print("What do you call a 'Pen' in Te Reo Maori?")
+        # pene
+        print()
+        print("QUESTION 2")
+        print("What do you call a 'Pencil' in Te Reo Maori?")
+        # pene rākau
+        print()
+        print("QUESTION 3")
+        print("What do you call 'Paper' in Te Reo Maori?")
+        # pepa
+        print()
+        print("QUESTION 4")
+        print("What do you call a 'Book' in Te Reo Maori?")
+        # pukapuka
+        print()
+        print("QUESTION 5")
+        print("What do you call a 'Window' in Te Reo Maori?")
+        # matapihi
+        print()
+        print("QUESTION 6")
+        print("What do you call a 'Door' in Te Reo Maori?")
+        # kūaha
+        print()
+        print("QUESTION 7")
+        print("What do you call a 'Chair' in Te Reo Maori?")
+        # tūru
+      elif quizlevel == 3:
+        print("Maori Places/Geography quiz has begun.")
+        print()
+        print("QUESTION 1")
+        print("What does 'Maunga' mean in English? - E.g. Mount Maunganui")
+        # Mountain
+        print()
+        print("QUESTION 2")
+        print("What does 'Roto' mean in English? - E.g. Rotorua")
+        # Lake
+        print()
+        print("QUESTION 3")
+        print("What does 'Awa' mean in English? - E.g. Te Awa")
+        # River / Valley
+        print()
+        print("QUESTION 4")
+        print("What does 'Whanga' mean in English? - E.g. Whanganui")
+        # Bay
+        print()
+        print("QUESTION 5")
+        print("What do you call a 'Hill' in Te Reo Maori?")
+        # Puke
+        print()
+        print("QUESTION 6")
+        print("What do you call the 'Sea' in Te Reo Maori")
+        # Moana
+        print("QUESTION 7")
+        print("What do you call an 'Island' in Te Reo Maori?")
+        # Motu
+      elif quizlevel == 4:
+        print("Maori Proverbs quiz has begun.")
+        print()
+        print("QUESTION 1")
+        print("How do you say 'Be a leader not a follower' in Te Reo Maori?")
+        # Haere taka mua, taka muri; kaua e whai
+        print()
+        print("QUESTION 2")
+        print("How do you say 'Calm after the storm' in Te Reo Maori?")
+        # He maonga āwhā
+        print()
+        print("QUESTION 3")
+        print("How do you say 'It is the thought that counts' in Te Reo Maori?")
+        # Ahakoa he iti kete, he iti nā te aroha
+        print()
+        print("QUESTION 4")
+        print("How do you say 'Better times are coming' in Te Reo Maori?")
+        # He rā ki tua
+        print()
+        print("QUESTION 5")
+        print("How do you say 'What is done is done' in Te Reo Maori?")
+        # E kore a muri e hokia
+        print()
+        print("QUESTION 6")
+        print("What does 'Aroha mai, aroha atu' mean in English?")
+        # Love received demands love returned
+        print()
+        print("QUESTION 7")
+        print("What does 'E kore e mimiti te aroha mōu' mean in English?")
+        # My love for you will never wane
+      elif quizlevel == 5:
+        print("Maori Sentences - Commands quiz has begun.")
+        print()
+        print("QUESTION 1")
+        print("How do you tell someone to 'Stand' in Teo Reo Maori?")
+        # E tū
+        print()
+        print("QUESTION 2")
+        print("How do you tell someone to 'Sit' in Te Reo Maori?")
+        # E noho
+        print()
+        print("QUESTION 3")
+        print("How do you tell someone 'You should go home' in Te Reo Maori?")
+        # Me haere koe ki te kāinga
+        print()
+        print("QUESTION 4")
+        print("How do you tell someone 'You should wash the clothes' in Te Reo Maori?")
+        # Me horoi ngā kākahu
+        print()
+        print("QUESTION 5")
+        print("How do you demand someone to 'Wash the clothes' in Te Reo Maori?")
+        # Horoia ngā kākahu
+        print()
+        print("QUESTION 6")
+        print("How do you tell someone 'Let's go to the shop' in Te Reo Maori?")
+        # Ka haere tāua ki te toa
+        print("QUESTION 7")
+        print("What does 'Haere mai' mean in English?")
+        # Come here
+      elif quizlevel == 6:
+        print("Maori Sentences - Questions quiz has begun.")
+        print()
+        print("QUESTION 1")
+        print("How do you say 'Yes' and 'No' in Te Reo Maori?")
+        # Āe + Kāo
+        print()
+        print("QUESTION 2")
+        print("How would you say 'The sun is shining' as a question in Te Reo Maori?")
+        # Kei te whiti te rā?
+        print()
+        print("QUESTION 3")
+        print("How do you say 'When did you return?' in Te Reo Maori?")
+        # Nōnahea koe i hoki mai ai?
+        print()
+        print("QUESTION 4")
+        print("How do you say 'Where are you from?' in Te Reo Maori?")
+        # Nō hea koe?
+        print()
+        print("QUESTION 5")
+        print("How do you say 'To where is he going?' in Te Reo Maori?")
+        # Ka haere ia ki hea?
+        print()
+        print("QUESTION 6")
+        print("How do you say 'What's this?' in Te Reo Maori?")
+        # He aha tēnei?
+        print()
+        print("QUESTION 7")
+        print("How do you ask 'Who are you?' in Te Reo Maori?")
+        # Ko wai koe?
+      elif quizlevel != range(1,10):
+        print("Sorry, that is not a quiz valid level.")
+        print("Make sure you are typing only the number of the level.")
+        quiz()
+      
     quiz()
   elif cont2 != "Documents" or cont2 != "documents" or cont2 != "quiz" or cont != "Quiz":
     print("Sorry " + cont2 + " is not a valid answer. Please try again... ")
@@ -281,7 +483,7 @@ def ins():
     print("Sorry, " + str(instructionscontinue()) + " is not a valid answer.")
   
 """
-Starting Welcome - v1.0
+Starting Welcome (Phase 1) - v1.0
 Authorised by Liam Pettigrew
 17th June 2021
 
